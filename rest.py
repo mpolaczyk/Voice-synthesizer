c11008c6db7fdcef21dd44299715d0e2780551f0
mpolaczyk <marcin.polaczyk@gmail.com>
2014-09-24 00:36:16 +0200

    Rest service with form done.

--- rest.py
+++ rest.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import web
+
 from subprocess import Popen, PIPE, call
 
 urls = (
     '/sayhello/(.*)', 'sayhello',
-    '/listv', 'listv'
+    '/listv', 'listv',
+	'/say', 'say'
     )
 
 app = web.application(urls, globals())
 
+render = web.template.render('templates/')
 
 class Synth(object):
     synthPath = "./bin/synth"
@@ -23,7 +26,7 @@ class Synth(object):
     def GetVoices():
         p = Popen([Synth.synthPath, '--listv'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         output, err = p.communicate()
-        return [output, p.returncode, err]
+        return filter(None, output.split('\n'))
 
 
 class sayhello:
@@ -33,6 +36,27 @@ class sayhello:
 class listv:
     def GET(self):
         return Synth.GetVoices()
+
+sayForm = web.form.Form(
+    web.form.Dropdown('voice', Synth.GetVoices()),
+    web.form.Textbox('text'),
+    web.form.Button('Say')
+)
+
+class say:
+    def GET(self):
+        f = sayForm()
+        return render.say(content=f)
+
+    def POST(self):
+        f = sayForm()
+        if not f.validates(): 
+            return render.formtest(f)
+        else:
+            Synth.Say(f.d.voice, f.d.text)
+            return f.d.voice + " just said: " + f.d.text
+
+application = app.wsgifunc()
 
 if __name__ == "__main__":
     app.run()
